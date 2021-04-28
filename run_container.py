1f6133546eec3f9242b75e4ae36df17ea7a3f50a
polotto <angelopolotto@gmail.com>
2021-04-28 19:42:05 -0300

    run docker changed

--- run_container.py
+++ run_container.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-from shell import exec
+import subprocess
+import os
+
+def cmd(cmd, start_session=False):
+    if start_session:
+        os.system(cmd)
+        return (None, None, None)
+    else:
+        pipes = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        std_out, std_err = pipes.communicate()
+        return (pipes.returncode != 0, std_out.strip(), std_err.strip())
 
 name='kali'
 
-(error, _, _) = exec.cmd(f'docker container top {name}')
+(error, _, _) = cmd(f'docker container top {name}')
 
 if error:
-    (_, std, err) = exec.cmd(f'docker container run --rm --name {name} -v $(pwd)/docs:/root/docs --detach --tty polotto/kali')
+    (_, std, err) = cmd(f'docker container run --rm --name {name} -v $(pwd)/docs:/root/docs --detach --tty polotto/kali')
     print(std)
     print(err)
 
-exec.cmd(f'docker container exec --tty --interactive {name} /bin/bash', start_session=True)
+cmd(f'docker container exec --tty --interactive {name} /bin/bash', start_session=True)
